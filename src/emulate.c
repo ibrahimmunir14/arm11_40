4dba5b6804e9c32c5e97e7c2489a6fd713a1bf9d
Umer <suh18@ic.ac.uk>
2019-05-23 15:02:53 +0100

    Reading filename from arguments

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,24 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
+  if (argc != 2) {
+    return EXIT_FAILURE;
+  }
+  FILE *fPointer;
+  char *fileName = argv[1];
+  fPointer = fopen(fileName, "r");
+
+  fclose(fPointer);
+
   return EXIT_SUCCESS;
 }
+
+
+/* CHECKLIST
+ *
+ * 1. from the input, take each line and put it in an index of an array (how do we find the size?)
+ * 2. define a struct which includes the pipeline (decode and execute variables), the array of registers, and all words in memory
+ * 3. process each type of the 4 instructions with its separate function
+ *
+ * */
