b2eed26139d62f3520f00a7149a5d3ba1659d6bf
Ibrahim Munir-Zubair <im318@ic.ac.uk>
2019-05-24 02:45:46 +0100

    Implemented main Pipeline Loop for fetch-decode-execute. Execution is delegated to executeInstruction. Added appropriate helper methods for pipeline loop. Added comments and print stmts for testing/development purposes.

--- src/emulate.c
+++ src/emulate.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <math.h>
+#include <stdbool.h>
 
 // define types to aid readability
 typedef uint32_t REGISTER;	// registers are 32 bits
@@ -9,6 +10,7 @@ typedef uint32_t WORD;		// words are 32 bits
 typedef uint16_t ADDRESS;   // addresses are 16 bits
 typedef uint8_t BYTE;		// bytes are 8 bits
 
+// define constants related to registers
 // store registers in an array of register (R13=SP; R14=LR; R15=PC; R16=CPSR)
 // store memory as array of byte, 64kb memory capacity, 1 word is 4 bytes
 #define NUM_REG 17
@@ -31,15 +33,16 @@ enum OpCode {AND=0, EOR=1, SUB=2, RSB=3, ADD=4, TST=8, TEQ=9, CMP=10, ORR=12, MO
 
 // function declarations
 WORD readWord(ADDRESS startAddress);
+void executeInstruction(WORD inst);
+void incrementPC(void);
 void printResults(void);
 
 int main(int argc, char **argv) {
     // ensure we have one argument, the filename
     if (argc != 2) { return EXIT_FAILURE; }
 
-    // initialize registers to 0s
+    // initialize registers, memory to 0s
     for (int i = 0; i < NUM_REG; i++) { registers[i] = 0; }
-    // initialize memory to 0s
     for (int i = 0; i < MEM_SIZE; i++) { memory[i] = 0; }
 
     // set up memory array with instructions
@@ -62,8 +65,54 @@ int main(int argc, char **argv) {
     }
     printf("\n");
 
+    // main pipeline loop
+    WORD instrToExecute = 0;
+    WORD instrToDecode = 0;
+    bool hasInstrToExecute = false;
+    bool hasInstrToDecode = false;
+    while (registers[REG_PC] < MEM_SIZE) {
+        // execute instrToExecute
+        if (hasInstrToExecute) {
+            if (instrToExecute == 0) {
+                printf("TERMINATE: All-0 Instruction\n\n");
+                break; // terminate on all-0 instruction
+            } else {
+                executeInstruction(instrToExecute);
+                printf("Executed Instruction 0x%08x\n", instrToExecute);
+            }
+        }
+
+        // decode instrToDecode and put it in instrToExecute
+        // note: decoding is actually done during execution
+        if (hasInstrToDecode) {
+            hasInstrToExecute = true;
+            instrToExecute = instrToDecode;
+            printf("Decoded Instruction 0x%08x\n", instrToExecute);
+
+        }
+
+        // fetch next instruction and put it in instrToDecode
+        instrToDecode = readWord(registers[REG_PC]);
+        printf("Fetched Instruction at 0x%08x : 0x%08x\n", registers[REG_PC], instrToDecode);
+        hasInstrToDecode = true;
+
+        // increment program counter
+        incrementPC();
+        printf("Incremented PC to 0x%08x\n\n", registers[REG_PC]);
+    }
+
     printResults();
     return EXIT_SUCCESS;
+}
+
+// decode the given instruction and delegate to appropriate helper functions
+void executeInstruction(WORD instr) {
+    // TODO: Implement this function to decode instruction and delegate to appropriate functions
+}
+
+// increment the PC register to the address of the next word instruction
+void incrementPC(void) {
+    registers[REG_PC] += 4;
 }
 
 // output the contents of registers and non-zero memory
