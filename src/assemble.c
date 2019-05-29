512aa0da9c1e1be1f12704b410f604af6dc9d80a
umerhasan17 <umerhasan17@gmail.com>
2019-05-29 19:33:51 +0100

    Creating a binary file writer. To be tested with proper file paths and check for new line creation

--- src/assemble.c
+++ src/assemble.c
@@ -1,5 +1,24 @@
-#include <stdlib.h>
+#include "assemble.h"
 
 int main(int argc, char **argv) {
   return EXIT_SUCCESS;
+  // TODO function to get the number of lines in the input file
+}
+
+void binaryFileWriter(WORD* instructions) {
+    FILE * fPointer;
+    // To create a new file use wb, to append to a file use ab
+    fPointer = fopen("test.bin", "wb");
+    int size = sizeof(instructions) / sizeof(WORD);
+    for (int i = 0; i < size; i ++) {
+        unsigned int n = instructions[i];
+        unsigned char bytes[4];
+        bytes[0] = (n >> 24) & 0xFF;
+        bytes[1] = (n >> 16) & 0xFF;
+        bytes[2] = (n >> 8) & 0xFF;
+        bytes[3] = n & 0xFF;
+        fwrite(bytes, sizeof(bytes), 1, fPointer);
+        fwrite("\n", sizeof(char), 1, fPointer);
+    }
+    fclose(fPointer);
 }
