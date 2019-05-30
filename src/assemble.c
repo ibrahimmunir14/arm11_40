f37b29cd0bdc992ebfef2b2b683616439e166503
Umer <suh18@ic.ac.uk>
2019-05-30 18:54:20 +0100

    Testing tokenizer and getting instruction opcode from string

--- src/assemble.c
+++ src/assemble.c
@@ -1,14 +1,80 @@
 #include "assemble.h"
 #include "fileIO.h"
+#include <regex.h>
+#include <string.h>
 
 // TODO move this function into binary ops and maybe create a nibble type for value
+void tokenize(char* line);
 
 int main(int argc, char **argv) {
+
+    char buff[80];
+    strcpy (buff, "mov r1 r0");
+    tokenize(buff);
+//    printf("hello world!\n");
+//    char* line1;
+//    line1 = "mov something r1 something";
+    encodeInstruction(buff);
+//    printf("finished\n");
   return EXIT_SUCCESS;
   // TODO function to get the number of lines in the input file
   // TODO how to store 17 registers with 4 bits, which register left out
 }
 
+
+
+int match(const char *string, const char *pattern)
+{
+    regex_t re;
+    if (regcomp(&re, pattern, REG_EXTENDED|REG_NOSUB) != 0) return 0;
+    int status = regexec(&re, string, 0, NULL, 0);
+    regfree(&re);
+    if (status != 0) return 0;
+    return 1;
+}
+
+void tokenize(char* line)
+{
+    char* cmd = strtok(line," ");
+
+    while (cmd != NULL)
+    {
+        printf ("%s\n",cmd);
+        cmd = strtok(NULL, " ");
+    }
+}
+
+WORD encodeInstruction(char* line) {
+    WORD value = 0;
+    const char branchPattern[] = "b*";
+    const char mPattern[] = "m*";
+
+    char* token = strtok(line," ");
+
+    while (token != NULL)
+    {
+        printf ("%s\n",token);
+        token = strtok(NULL, " ");
+    }
+
+    if (match(token, branchPattern)) {
+        printf("matching on branch");
+    } else if (match(token, mPattern)) {
+        printf("matching on multiply or mov");
+    } else if (match(token, "andeq")) {
+        printf("matching on andeq");
+    } else if (match(token, "lsl")) {
+        printf("matching on lsl");
+    } else if (match(token, "ldr")) {
+        printf("matching on ldr");
+    } else if (match(token, "str")) {
+        printf("matching on str");
+    } else {
+        printf("matching on dataproc");
+    }
+
+    return value;
+}
 
 
 WORD assembleMultiply(REGNUMBER rd, REGNUMBER rm, REGNUMBER rs, REGNUMBER rn, bool aFlag) {
